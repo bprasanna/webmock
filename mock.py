48417afcc5038617b89a2454f1adc782c0d54e4a
bprasanna <bprasannakumar@gmail.com>
2017-03-28 08:08:58 +0530

    1. mock.py 1.1. Added support for url parsing using urlparse from urllib.parse 1.2. Added conditional rendering of index.html with parameter based on validated url 2. index.html 2.1. Added form control with submit button 2.2. Conditional display of output based on the url value

--- mock.py
+++ mock.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from urllib.parse import urlparse
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mock():
-    return render_template('index.html')
+	if request.method == 'POST':
+	    url = request.form['url']
+	    paurl = urlparse(url)
+	    if paurl.netloc != '':
+	        return render_template('index.html',url=url)	
+	    else:
+	        return render_template('index.html',url='Invalid URL')	
+	else:
+		return render_template('index.html')
+    
